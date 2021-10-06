fcab0c48317ffe3b36916704b4a42ad78322722a
Joel Lajoie-Corriveau <joel.lajoie.corriveau@outlook.com>
2021-10-06 06:04:40 -0400

    Fixed grappling hook
    The grappling hook now fires at the mouse cursor properly.
    
    The issue was with a few things:
    - There was an offset on the Grappling Hook game object. This was offsetting all points.
    - When clicking, we took the vector from origin to the click. But that's not what we want. We want the vector that comes from the initial launch point of the grappling hook and goes to the mouse click. Simple vector math fixed this.

--- Stitches/Assets/Scripts/HookController.cs
+++ Stitches/Assets/Scripts/HookController.cs
@@ -12,7 +12,10 @@ public class HookController : MonoBehaviour
     private LineRenderer m_grapplingHookRenderer;
     private Vector3 m_clickPosition;
     private Vector3 m_currentPosition;
+    private Vector3 m_directionUnitVector;
     private BoxCollider2D m_hookCollider;
+
+    [SerializeField] private GameObject m_player;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,7 @@ public class HookController : MonoBehaviour
 
         m_currentPosition = new Vector3(0, 0, 0);
         m_clickPosition = new Vector3(0, 0, 0);
+        m_directionUnitVector = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
@@ -46,7 +50,7 @@ public class HookController : MonoBehaviour
 
             // Retracting hook
             m_grapplingHookOut = false;
-            m_hookCollider.transform.position = transform.TransformPoint(transform.position) + new Vector3(0, m_grapplingStartHeightOffset, 0);
+            m_hookCollider.gameObject.transform.position = m_player.transform.position + new Vector3(0, m_grapplingStartHeightOffset, 0);
         }
         else
         {
@@ -60,12 +64,16 @@ public class HookController : MonoBehaviour
             // Setting worldpos z to 1 so it doesnt draw behind the background.
             worldPosition.z = 1;
 
+            // Calculating direction vector
+            Vector3 direction = worldPosition - (m_player.transform.position + new Vector3(0, m_grapplingStartHeightOffset, 0));
+            m_directionUnitVector = direction / direction.magnitude;
+
             m_clickPosition = worldPosition;
-            m_currentPosition = transform.TransformPoint(transform.position) + new Vector3(0, m_grapplingStartHeightOffset, 0);
+            m_currentPosition = m_player.transform.position + new Vector3(0, m_grapplingStartHeightOffset, 0);
 
             m_grapplingHookOut = true;
 
-            m_hookCollider.transform.position = m_currentPosition;
+            m_hookCollider.gameObject.transform.position = m_player.transform.position + new Vector3(0, m_grapplingStartHeightOffset, 0);
             m_hookCollider.size = m_hookColliderSize;
 
             m_hookCollider.enabled = true;
@@ -81,33 +89,28 @@ public class HookController : MonoBehaviour
     }
 
     void HandleGrapplingHook()
-    {
+    {        
         // Handling display and collision
         if (m_grapplingHookOut)
         {
-            // Making a unit vector
-            Vector3 unitPosition = m_clickPosition / m_clickPosition.magnitude;
-            // Resetting Z pos to make sure we dont cumulate z later down the line.
-            unitPosition.z = 0;
-
             // Making a line
             m_grapplingHookRenderer.positionCount = 2;
 
             // We make the z position be 1 to make the points have a z position of 1, making them show up on our screen in front of the background.
             // We also add the vector 0, offset, 0 to make the grappling hook start above our character.
-            Vector3 firstPointPosition = transform.TransformPoint(transform.position) + new Vector3(0, m_grapplingStartHeightOffset, 0);
+            Vector3 firstPointPosition = m_player.transform.position + new Vector3(0, m_grapplingStartHeightOffset, 0);
             firstPointPosition.z = 1;
             m_grapplingHookRenderer.SetPosition(0,  firstPointPosition);
 
             // Increasing the length of the whole line
-            m_currentPosition = m_currentPosition + (unitPosition * m_grappleSpeed * Time.deltaTime);
+            m_currentPosition = m_currentPosition + (new Vector3(m_directionUnitVector.x, m_directionUnitVector.y, 0) * m_grappleSpeed * Time.deltaTime);
 
             Vector3 secondPointPosition = m_currentPosition;
             secondPointPosition.z = 1;
             m_grapplingHookRenderer.SetPosition(1, secondPointPosition);
 
             // Handle Hook Collision
-            m_hookCollider.transform.position = m_currentPosition;
+            m_hookCollider.transform.position = secondPointPosition;
         }
     }
 }
