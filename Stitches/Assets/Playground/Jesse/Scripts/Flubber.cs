42631a626a37776a4eec08f64e883549ee10c962
Clark <t.clark.dumoulin@gnail.com>
2021-10-24 13:27:02 -0400

    making flubber ai for design scene

--- Stitches/Assets/Playground/Jesse/Scripts/Flubber.cs
+++ Stitches/Assets/Playground/Jesse/Scripts/Flubber.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Flubber : MonoBehaviour
 {
+    public Vector2 worldPoint = new Vector2(27, 13);
+    public float agroRadius;
     public float interval;
-    private Rigidbody2D RB;
-    private float lastForce = 0;
     public float force = 15;
-    public GameObject player;
-    public SpriteRenderer SR;
+
+
+    private float lastForce = 0;
+    private SpriteRenderer SR;
+    private Rigidbody2D RB;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +26,32 @@ public class Flubber : MonoBehaviour
     void Update()
     {
         //FlubberOne();
-        FlubberTwo();
-        FlubberThree();
+        //FlubberTwo();
+        //FlubberThree();
+
+        //flys around world point until player comes in proximity
+        FlubberFour();
         flipDirection();
     }
 
-    private void FlubberThree()
+    private void FlubberFour()
     {
-        transform.Rotate(Vector3.up , Space.Self);
+        LayerMask mask = LayerMask.GetMask("Player");
+        Collider2D player = Physics2D.OverlapCircle((Vector2)transform.position, agroRadius, mask);
+        Vector2 targetPoint = worldPoint;
+        if (Physics2D.OverlapCircle((Vector2)transform.position, agroRadius, mask) != null) 
+        {
+            if ((player.transform.position - this.transform.position).magnitude < 20)
+            {
+                targetPoint = player.transform.position;
+            }
+        }
+        if (Time.timeSinceLevelLoad - lastForce >= interval)
+        {
+            RB.AddForce((targetPoint  - (Vector2)this.transform.position).normalized * force, ForceMode2D.Impulse);
+            lastForce = Time.timeSinceLevelLoad;
+        }
     }
-
     private void flipDirection()
     {
         if (RB.velocity.x > 0)
@@ -44,6 +64,15 @@ public class Flubber : MonoBehaviour
         }
     }
 
+    //extra AIs 
+
+    /*
+    private void FlubberThree()
+    {
+        transform.Rotate(Vector3.up , Space.Self);
+    }
+
+   
     private void FlubberTwo()
     {
         if (Time.timeSinceLevelLoad - lastForce >= interval)
@@ -69,5 +98,5 @@ public class Flubber : MonoBehaviour
             RB.AddForce(Vector3.up * force, ForceMode2D.Impulse);
             lastForce = Time.timeSinceLevelLoad;
         }
-    }
+    }*/
 }
