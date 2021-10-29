75c6da0b794fd2c1d9eebd29b8d57dfb30326cfe
Joel Lajoie-Corriveau <joel.lajoie.corriveau@outlook.com>
2021-10-29 10:55:27 -0400

    Boost when Hooked and pulling
    - Added ability to boost the player when the player reels into a grappled enemy.
    
    - Made the enemy freeze when grappled, and proceed as normal when ungrappled.

--- Stitches/Assets/Playground/Jesse/Scripts/Flubber.cs
+++ Stitches/Assets/Playground/Jesse/Scripts/Flubber.cs
@@ -8,11 +8,14 @@ public class Flubber : MonoBehaviour
     public float agroRadius;
     public float interval;
     public float force = 15;
+    public bool m_isHooked = false;
 
 
     private float lastForce = 0;
     private SpriteRenderer SR;
     private Rigidbody2D RB;
+    private CircleCollider2D m_collider;
+    [SerializeField] private ParticleSystem m_explosionEffectSystem;
     
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class Flubber : MonoBehaviour
         RB = GetComponent<Rigidbody2D>();
         interval = 1.0f;
         SR = GetComponent<SpriteRenderer>();
+        m_collider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
@@ -34,6 +38,52 @@ public class Flubber : MonoBehaviour
         flipDirection();
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If we are hooked
+        if(m_isHooked)
+        {
+            // If it's the player that enters our body
+            if(collision.gameObject.CompareTag("Player"))
+            {
+                // We explode, giving the player a speedboost.
+                PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+                player.Boost();
+
+                // Trigger the particle effect
+                m_explosionEffectSystem.gameObject.transform.position = this.gameObject.transform.position;
+                m_explosionEffectSystem.Play();
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public void ToggleHook()
+    {
+        if(m_isHooked)
+        {
+            // Unhooking
+            m_isHooked = false;
+
+            // Leaving only rotation frozen.
+            RB.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+            // Reactivating collider
+            m_collider.isTrigger = false;
+        }
+        else
+        {
+            m_isHooked = true;
+
+            // Object becomes immovable.
+            RB.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
+
+            // Object cannot stop movement.
+            m_collider.isTrigger = true;
+        }
+    }
+
     private void FlubberFour()
     {
         LayerMask mask = LayerMask.GetMask("Player");
@@ -46,10 +96,14 @@ public class Flubber : MonoBehaviour
                 targetPoint = player.transform.position;
             }
         }
-        if (Time.timeSinceLevelLoad - lastForce >= interval)
+        // If we are hooked by the player, we cant move.
+        if(!m_isHooked)
         {
-            RB.AddForce((targetPoint  - (Vector2)this.transform.position).normalized * force, ForceMode2D.Impulse);
-            lastForce = Time.timeSinceLevelLoad;
+            if (Time.timeSinceLevelLoad - lastForce >= interval)
+            {
+                RB.AddForce((targetPoint - (Vector2)this.transform.position).normalized * force, ForceMode2D.Impulse);
+                lastForce = Time.timeSinceLevelLoad;
+            }
         }
     }
     private void flipDirection()
