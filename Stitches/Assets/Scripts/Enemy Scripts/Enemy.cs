309c368292da53ccd03e2406e0184cb40b4f6d6e
Clark <t.clark.dumoulin@gnail.com>
2021-11-06 00:10:38 -0400

    Restructured the scripts, we now have an enemy superclass

--- Stitches/Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Stitches/Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -4,15 +4,140 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer m_SR;
+    public Rigidbody2D m_RB;
+    public Animator m_anim;
+    public CircleCollider2D m_collider;
+    public bool m_isHooked = false;
+    public bool m_isVulnerable = false;
+    public bool m_aiIsOff = false;
+    public bool m_frozen = false;
+    protected float m_freezeElapsedTime = 0f;
+    protected bool m_thawing = false;
+    protected float m_vulnerabilityElapsedTime = 0f;
+    protected bool m_vulnerabilityIsOnTimer = false;
+    protected ParticleSystem m_explosionEffectSystem;
+    public float m_freezeTime = 1f;
+    public float m_vulnerabilityTime = 2f;
+
+    public void Start()
     {
+        m_explosionEffectSystem =  GameObject.Find("ExplosionEffect").GetComponent<ParticleSystem>();
+        m_SR = GetComponent<SpriteRenderer>();
+        m_RB = GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
+        m_collider = GetComponent<CircleCollider2D>();
+    }
+    public void Update()
+    {s
+        if (m_frozen && m_thawing)
+            m_freezeElapsedTime += Time.deltaTime;
+
+        if (m_thawing && m_freezeElapsedTime >= m_freezeTime)
+        {
+            Unfreeze();
+        }
+
+        if (m_isVulnerable && m_vulnerabilityIsOnTimer)
+            m_vulnerabilityElapsedTime += Time.deltaTime;
+
+        if (m_vulnerabilityIsOnTimer && m_vulnerabilityElapsedTime >= m_vulnerabilityTime)
+        {
+            ToggleVulnerability();
+
+            m_vulnerabilityIsOnTimer = false;
+        }
+    }
+    public void ToggleHook()
+    {
+        if (m_isHooked)
+        {
+            // Unhooking
+            m_isHooked = false;
+
+            // Checking if we need to unfreeze / Make ourselves strong again on a timer, or not.
+            if (m_vulnerabilityIsOnTimer)
+            {
+                // Player unhooked and boosted. We are vulnerable for some time.
+
+                // Starting the timer.
+                m_freezeElapsedTime = 0f;
+
+                m_thawing = true;
+            }
+            else
+            {
+                // Player unhooked without boosting. We get strong right away.
+                Unfreeze();
+                ToggleVulnerability();
+            }
+        }
+        else
+        {
+            m_isHooked = true;
+            Freeze();
+
+            ToggleVulnerability();
+        }
+
         
     }
+        public void Freeze()
+        {
+            m_frozen = true;
 
-    // Update is called once per frame
-    void Update()
+            // Object becomes immovable.
+            m_RB.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        public void Unfreeze()
+        {
+            m_frozen = false;
+
+            // Leaving only rotation frozen.
+            m_RB.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+    public void ToggleVulnerability()
     {
-        
+        m_isVulnerable = !m_isVulnerable;
+
+        if (m_isVulnerable)
+        {
+            // Object cannot stop movement.
+            m_collider.isTrigger = true;
+        }
+        else
+        {
+            m_collider.isTrigger = false;
+        }
+    }
+
+    public void StartVulnerabilityTimer()
+    {
+        m_vulnerabilityElapsedTime = 0f;
+        m_vulnerabilityIsOnTimer = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If it's the player that enters our body
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (m_isVulnerable)
+            {
+                // We explode, giving the player a speedboost.
+                PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+                player.RetractHook();
+                // Uncomment to make the player boost when coming into contact with a hooked enemy.
+                //player.Boost();
+
+                // Trigger the particle effect
+                m_explosionEffectSystem.gameObject.transform.position = this.gameObject.transform.position;
+                m_explosionEffectSystem.Play();
+
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
--- Stitches/Assets/Scripts/Enemy Scripts/Flubber.cs
+++ Stitches/Assets/Scripts/Enemy Scripts/Flubber.cs
@@ -2,68 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flubber : MonoBehaviour
+public class Flubber : Enemy
 {
     public Vector2 worldPoint = new Vector2(27, 13);
     public float agroRadius;
     public float interval;
     public float force = 15;
-    public bool m_isHooked = false;
-    public bool m_isVulnerable = false;
-    public bool m_aiIsOff = false;
-    public bool m_frozen = false;
+
     /// <summary>
     /// Time the enemy will stay frozen after the player unhooking from it.
     /// </summary>
-    public float m_freezeTime = 1f;
+    
     /// <summary>
     /// Time the enemy will stay vulnerable after the player unhooking from it.
     /// </summary>
-    public float m_vulnerabilityTime = 2f;
+    
 
     private float lastForce = 0;
-    private float m_freezeElapsedTime = 0f;
-    private bool m_thawing = false;
-    private float m_vulnerabilityElapsedTime = 0f;
-    private bool m_vulnerabilityIsOnTimer = false;
+    
 
-    private SpriteRenderer SR;
-    private Rigidbody2D RB;
-    private CircleCollider2D m_collider;
-    [SerializeField] private ParticleSystem m_explosionEffectSystem;
     
     // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
-        RB = GetComponent<Rigidbody2D>();
+        base.Start();
         interval = 1.0f;
-        SR = GetComponent<SpriteRenderer>();
-        m_collider = GetComponent<CircleCollider2D>();
-
         if (m_frozen)
             Freeze();
     }
 
     // Update is called once per frame
-    void Update()
+    new void Update()
     {
-        if(m_frozen && m_thawing)
-            m_freezeElapsedTime += Time.deltaTime;
-
-        if (m_thawing && m_freezeElapsedTime >= m_freezeTime)
-        {
-            Unfreeze();           
-        }
-
-        if (m_isVulnerable && m_vulnerabilityIsOnTimer)
-            m_vulnerabilityElapsedTime += Time.deltaTime;
-
-        if (m_vulnerabilityIsOnTimer && m_vulnerabilityElapsedTime >= m_vulnerabilityTime)
-        {
-            ToggleVulnerability();
-
-            m_vulnerabilityIsOnTimer = false;
-        }
+        base.Update();
 
         if (!m_aiIsOff)
         {
@@ -74,80 +45,13 @@ public class Flubber : MonoBehaviour
             //flys around world point until player comes in proximity
             FlubberFour();
         }
-        flipDirection();
+        FlipDirection();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // If it's the player that enters our body
-        if(collision.gameObject.CompareTag("Player"))
-        {
-            if(m_isVulnerable)
-            {
-                // We explode, giving the player a speedboost.
-                PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+    
 
-                player.RetractHook();
-                // Uncomment to make the player boost when coming into contact with a hooked enemy.
-                //player.Boost();
+    
 
-                // Trigger the particle effect
-                m_explosionEffectSystem.gameObject.transform.position = this.gameObject.transform.position;
-                m_explosionEffectSystem.Play();
-
-                Destroy(this.gameObject);
-            }          
-        }
-    }
-
-    public void ToggleHook()
-    {
-        if(m_isHooked)
-        {
-            // Unhooking
-            m_isHooked = false;
-            
-            // Checking if we need to unfreeze / Make ourselves strong again on a timer, or not.
-            if(m_vulnerabilityIsOnTimer)
-            {
-                // Player unhooked and boosted. We are vulnerable for some time.
-
-                // Starting the timer.
-                m_freezeElapsedTime = 0f;
-
-                m_thawing = true;
-            }
-            else
-            {
-                // Player unhooked without boosting. We get strong right away.
-                Unfreeze();
-                ToggleVulnerability();
-            }            
-        }
-        else
-        {
-            m_isHooked = true;
-            Freeze();
-
-            ToggleVulnerability();
-        }
-    }
-
-    public void Freeze()
-    {
-        m_frozen = true;
-
-        // Object becomes immovable.
-        RB.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
-    }
-
-    public void Unfreeze()
-    {
-        m_frozen = false;
-
-        // Leaving only rotation frozen.
-        RB.constraints = RigidbodyConstraints2D.FreezeRotation;
-    }
 
     private void FlubberFour()
     {
@@ -166,53 +70,30 @@ public class Flubber : MonoBehaviour
         {
             if (Time.timeSinceLevelLoad - lastForce >= interval)
             {
-                RB.AddForce((targetPoint - (Vector2)this.transform.position).normalized * force, ForceMode2D.Impulse);
+                m_RB.AddForce((targetPoint - (Vector2)this.transform.position).normalized * force, ForceMode2D.Impulse);
                 lastForce = Time.timeSinceLevelLoad;
             }
         }
     }
-    private void flipDirection()
+    private void FlipDirection()
     {
-        if (RB.velocity.x > 0)
+        if (m_RB.velocity.x > 0)
         {
-            SR.flipX = true;
+            m_SR.flipX = true;
         }
         else
         {
-            SR.flipX = false;
+            m_SR.flipX = false;
         }
     }
 
-    public void ToggleVulnerability()
-    {
-        m_isVulnerable = !m_isVulnerable;
-
-        if(m_isVulnerable)
-        {
-            // Object cannot stop movement.
-            m_collider.isTrigger = true;
-        }
-        else
-        {
-            m_collider.isTrigger = false;
-        }
-    }
-
-    public void StartVulnerabilityTimer()
-    {
-        m_vulnerabilityElapsedTime = 0f;
-        m_vulnerabilityIsOnTimer = true;
-    }
+   
 
     //extra AIs 
 
     /*
-    private void FlubberThree()
-    {
-        transform.Rotate(Vector3.up , Space.Self);
-    }
+ 
 
-   
     private void FlubberTwo()
     {
         if (Time.timeSinceLevelLoad - lastForce >= interval)
