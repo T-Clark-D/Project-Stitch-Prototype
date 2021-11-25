9d5d62021b2bf306bb537c926790cdfbfa051b0b
Derek <dereklamtsangon@gmail.com>
2021-11-24 21:19:40 -0500

    Flubber goes to player and does a gust of wind

--- Stitches/Assets/Scripts/Enemy Scripts/Flubber.cs
+++ Stitches/Assets/Scripts/Enemy Scripts/Flubber.cs
@@ -4,35 +4,48 @@ using UnityEngine;
 
 public class Flubber : Enemy
 {
-    public Vector2 worldPoint = new Vector2(27, 13);
+    [SerializeField] public Vector2 worldPoint;
+    [SerializeField] public float interval;
     public float agroRadius;
-    public float interval;
+
     public float force = 15;
 
     /// <summary>
     /// Time the enemy will stay frozen after the player unhooking from it.
     /// </summary>
-    
+
     /// <summary>
     /// Time the enemy will stay vulnerable after the player unhooking from it.
     /// </summary>
-    
-
+    /// 
     private float lastForce = 0;
-    
 
-    
+    [SerializeField] Transform mTarget;
+    [SerializeField] float mFollowSpeed;
+    [SerializeField] float mFollowRange;
+    [SerializeField] float mStopRange;
+    [SerializeField] GameObject mGustPrefab;
+    [SerializeField] float mGustRecoil;
+
+    bool mInRange;
+    bool gustShot;
+    float mTime;
+    float gustDuration = 0.75f;
+    Rigidbody2D mRigidBody2D; // Temporary
+
+    Vector2 mFacingDirection;
+
     // Start is called before the first frame update
-    new void Start()
+    protected override void Start()
     {
         base.Start();
-        interval = 1.0f;
-        if (m_frozen)
-            Freeze();
+        mRigidBody2D = GetComponent<Rigidbody2D>();
+        /*if (m_frozen)
+            Freeze();*/
     }
 
     // Update is called once per frame
-    new void Update()
+    protected override void Update()
     {
         base.Update();
 
@@ -46,19 +59,73 @@ public class Flubber : Enemy
             FlubberFour();
         }
         FlipDirection();
+        if (mTarget != null)
+        {
+            FollowPlayer();
+        }
+        if (mInRange)
+        {
+            mTime += Time.deltaTime;
+            GustPlayer();
+            
+        }
+        if(gustShot)
+        {
+            mTime += Time.deltaTime;
+            if (mTime > gustDuration)
+            {
+                mRigidBody2D.velocity = Vector3.zero;
+                mRigidBody2D.Sleep();
+                gustShot = false;
+            }
+        }
+
     }
 
-    
+    private void FollowPlayer()
+    {
+        if ((Vector3.Distance(transform.position, mTarget.position)) <= mFollowRange && (Vector3.Distance(transform.position, mTarget.position)) >= mStopRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mTarget.position, mFollowSpeed * Time.deltaTime);
+            
+        }
+        if ((Vector3.Distance(transform.position, mTarget.position)) <= mStopRange)
+        {
+            mInRange = true;
+        }
+        else
+        {
+            mInRange = false;
+            
+        }
+    }
 
-    
+    private void GustPlayer()
+    {
+        gustShot = true;
+        Vector2 targetPoint = mTarget.position;
+        GameObject gustObj = Instantiate(mGustPrefab, transform.position, transform.rotation);
+        FlubberGust gustRef = gustObj.GetComponent<FlubberGust>();
+        Vector2 shootDir = (targetPoint - (Vector2)this.transform.position).normalized;
+        gustRef.SetDirection(shootDir);
 
+        //m_RB.velocity = -shootDir * mGustRecoil;
+        mRigidBody2D.velocity = -shootDir * mGustRecoil;
+        mTime = 0.0f;
+    }
+
+    private void FaceDirection(Vector2 direction)
+    {
+        mFacingDirection = direction;
+        GetComponent<SpriteRenderer>().flipX = direction != Vector2.right;
+    }
 
     private void FlubberFour()
     {
         LayerMask mask = LayerMask.GetMask("Player");
         Collider2D player = Physics2D.OverlapCircle((Vector2)transform.position, agroRadius, mask);
         Vector2 targetPoint = worldPoint;
-        if (player != null) 
+        if (player != null)
         {
             if ((player.transform.position - this.transform.position).magnitude < agroRadius)
             {
@@ -66,7 +133,7 @@ public class Flubber : Enemy
             }
         }
         // If we are hooked by the player, we cant move.
-        if(!m_isHooked)
+        if (!m_isHooked)
         {
             if (Time.timeSinceLevelLoad - lastForce >= interval)
             {
@@ -75,25 +142,35 @@ public class Flubber : Enemy
             }
         }
     }
+
     private void FlipDirection()
     {
-        if (m_RB.velocity.x > 0)
+
+        if (transform.position.x > mTarget.position.x)
+        {
+            FaceDirection(Vector2.right);
+        }
+        else
+        {
+            FaceDirection(-Vector2.right);
+        }
+
+        /*if (m_RB.velocity.x > 0)
         {
             m_SR.flipX = true;
         }
         else
         {
             m_SR.flipX = false;
-        }
+        }*/
     }
 
-   
+
 
     //extra AIs 
 
     /*
  
-
     private void FlubberTwo()
     {
         if (Time.timeSinceLevelLoad - lastForce >= interval)
@@ -102,7 +179,6 @@ public class Flubber : Enemy
             lastForce = Time.timeSinceLevelLoad;
         }
     }
-
     private void FlubberOne()
     {
         if (Time.timeSinceLevelLoad - lastForce >= interval)
