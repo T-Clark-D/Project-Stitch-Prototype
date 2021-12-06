10fc6d20f8e99292c9fac73252b279750ed8769a
Clark <t.clark.dumoulin@gnail.com>
2021-12-06 16:30:08 -0500

    put light triggers on bonfire and implemented hoppy

---
+++ Stitches/Assets/LightColorBonfireReset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightColorBonfireReset : MonoBehaviour
+{
+    /*
+    public Color areaColor;
+    public Light directionalLight;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        directionalLight.color = areaColor;
+    }*/
+}
